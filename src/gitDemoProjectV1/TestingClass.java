f9158ba994afb31d50285e09a82227565ac6ded0
kelvo <kelvo@DESKTOP-AIDV1CG.attlocal.net>
2019-11-27 21:17:51 -0500

    First commit

---
+++ src/gitDemoProjectV1/TestingClass.java
@@ -0,0 +1,20 @@
+package gitDemoProjectV1;
+
+public class TestingClass {
+	String testName;
+	String testType;
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public String getTestType() {
+		return testType;
+	}
+	public void setTestType(String testType) {
+		this.testType = testType;
+	}
+	
+	
+}
